5a0fa3b6342233a0bbba7b7f329a8408baae3bae
Mraimundo <mdumundo18@gmail.com>
2023-08-04 23:13:35 -0300

    feat: initial setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { Provider as ReduxProvider } from "react-redux";
+import { store } from "./store";
+export function App() {
+  return (
+    <ReduxProvider store={store}>
+      <h1>Redux</h1>;
+    </ReduxProvider>
+  );
+}
